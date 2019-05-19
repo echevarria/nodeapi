f384ad4b98615958e32f33b6da31f99d3a6df829
quinho <quinho@gmail.com>
2019-05-19 18:07:28 -0300

    criação do recurso produto consumindo o recurso produto utilizando um array para armazenamento

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var index = require("./routes/index");
+var produto = require("./routes/produto");
 
 app.use("/", index);
+app.use("/produto", produto);
 
 app.listen(3000, function() {
   console.log("Servidor rodando! Acesse: http://localhost:3000");
