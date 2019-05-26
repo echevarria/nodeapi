5b3d2d6a28dc8aedfa5e57be9b9b70483587e383
quinho <quinho@gmail.com>
2019-05-26 15:34:42 -0300

    mantendo o código básico da api sem as rotas

--- server.js
+++ server.js
@@ -6,9 +6,12 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var index = require("./routes/index");
-
-app.use("/", index);
+app.get("/", function (request, response) {
+  response.status(200).send({
+    title: "Node Express API Top",
+    version: "1.0.0"
+  });
+});
 
 app.listen(3000, function() {
   console.log("Servidor rodando! Acesse: http://localhost:3000");
