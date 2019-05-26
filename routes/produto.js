3d024945ab8f3b0185fd5d8730b44374860d44ef
quinho <quinho@gmail.com>
2019-05-26 19:14:09 -0300

    adicionando mysql na api para persistir os dados

--- routes/produto.js
+++ routes/produto.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require("mysql");
 
 const app = express();
+
+const conexao = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "mysql",
+  database: "api"
+});
 
 const produtos = [
   {
@@ -18,64 +26,72 @@ const produtos = [
   }
 ];
 
+conexao.connect();
+
 app.get("/", function(request, response) {
-  response.status(200).send(produtos);
+  conexao.query("SELECT * FROM produtos", function(error, rows) {
+    if (error) {
+      response.status(500).send(error);
+    }
+    response.status(200).send(rows);
+  });
 });
 
 app.get("/:id", function(request, response) {
-  let produto = produtos.find(
-    produto => produto.id === parseInt(request.params.id)
+  conexao.query(
+    "SELECT * FROM produtos where id = " + parseInt(request.params.id),
+    function(error, rows) {
+      if (error) {
+        response.status(500).send(error);
+      }
+      if (rows.length > 0) {
+        response.status(200).send(rows);
+      } else {
+        response.status(404).send("Not Found");
+      }
+    }
   );
-
-  if (produto) {
-    response.status(200).send(produto);
-  } else {
-    response.status(404).send("Not Found");
-  }
 });
 
 app.post("/", function(request, response) {
-  let proximo = produtos.length + 1;
-
-  let novo = {
-    id: proximo,
-    descricao: request.body.descricao
-  };
-
-  produtos.push(novo);
-
-  response.status(201).send(novo);
+  conexao.query(
+    "INSERT INTO produtos (descricao) values ('" +
+      request.body.descricao +
+      "')",
+    function(error, rows) {
+      if (error) {
+        response.status(500).send(error);
+      }
+      response.status(201).send("");
+    }
+  );
 });
 
 app.put("/:id", function(request, response) {
-  let produto = produtos.find(
-    produto => produto.id === parseInt(request.params.id)
+  conexao.query(
+    "UPDATE produtos set descricao = '" +
+      request.body.descricao +
+      "' where id = " +
+      parseInt(request.params.id),
+    function(error, rows) {
+      if (error) {
+        response.status(500).send(error);
+      }
+      response.status(204).send("");
+    }
   );
-
-  if (produto) {
-    let alterado = {
-      id: produto.id,
-      descricao: request.body.descricao
-    };
-    let index = produtos.indexOf(produto);
-    produtos.splice(index, 1, alterado);
-    response.status(204).send(alterado);
-  } else {
-    response.status(404).send("Not Found");
-  }
 });
 
 app.delete("/:id", function(request, response) {
-  let produto = produtos.find(
-    produto => produto.id === parseInt(request.params.id)
+  conexao.query(
+    "DELETE FROM produtos where id = " + parseInt(request.params.id),
+    function(error, rows) {
+      if (error) {
+        response.status(500).send(error);
+      }
+      response.status(204).send("");
+    }
   );
-
-  if (produto) {
-    let index = produtos.indexOf(produto);
-    produtos.splice(index, 1);
-  }
-
-  response.status(204).send("DELETE");
 });
 
 module.exports = app;
