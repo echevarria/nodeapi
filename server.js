07483f4497a8b2e666c13238aecd6fb50bb9bd37
quinho <quinho@gmail.com>
2019-06-24 19:42:12 -0300

    liberando o cors

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var index = require("./routes/index");
 var produto = require("./routes/produto");
 
